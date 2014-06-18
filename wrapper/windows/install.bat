cacdf40fed9ed2426c9e7f600ab0dc77aa587ed4
wjcdx <wjcdx@qq.com>
2014-06-18 16:36:35 +0800

    [win:package] add a batch script to make a package

---
+++ wrapper/windows/install.bat
@@ -0,0 +1,1 @@
+C:\Windows\system32\regsvr32.exe -u x64\Release\TextService.dll
---
+++ wrapper/windows/package.bat
@@ -0,0 +1,31 @@
+::make a install package
+@echo off
+set PkgDir=XHua
+
+call :RstDir %PkgDir%
+copy install.bat %PkgDir%
+
+::install dlls
+set BinDir=%PkgDir%
+copy Release\TextService-x86.dll %BinDir%
+copy x64\Release\TextService-x86_64.dll %BinDir%
+
+::install dictionaries
+set DictDstDir=%PkgDir%\dict
+set DictSrcDir=..\..\src\dict
+call :RstDir %DictDstDir%
+copy %DictSrcDir%\dict3_sc.bin.xh %DictDstDir%
+copy %DictSrcDir%\lm_sc.t3g.xh %DictDstDir%
+
+goto Done
+
+:RstDir
+set Dir=%1
+if exist %Dir% (
+  rd /s /q %Dir%
+)
+md %Dir%
+goto :eof
+
+:Done
+pause
