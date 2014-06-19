e89f909e004005a082de39ababa0ca7f5127e0b7
wjcdx <wjcdx@qq.com>
2014-06-20 01:07:22 +0800

    [win] install resource files with a specified dir and fixed file names

--- wrapper/windows/package.bat
+++ wrapper/windows/package.bat
@@ -7,28 +7,28 @@ copy install.bat %PkgDir%
 
 ::install dlls
 set BinDir=%PkgDir%
-copy Release\TextService-x86.dll %BinDir%
-copy x64\Release\TextService-x86_64.dll %BinDir%
+copy Release\TextService-x86.dll "%BinDir%"
+copy x64\Release\TextService-x86_64.dll "%BinDir%"
 
 ::install dictionaries
-set DictDstDir=%PkgDir%\dict
+set DictDstDir=%PkgDir%\dict\xh
 set DictSrcDir=..\..\src\dict
 call :RstDir %DictDstDir%
-copy %DictSrcDir%\dict3_sc.bin.xh %DictDstDir%
-copy %DictSrcDir%\lm_sc.t3g.xh %DictDstDir%
+copy "%DictSrcDir%\dict3_sc.bin.xh" "%DictDstDir%\dict3_sc.bin"
+copy "%DictSrcDir%\lm_sc.t3g.xh" "%DictDstDir%\lm_sc.t3g"
 
 ::install configurations
 set ConfDstDir=%PkgDir%\config
 set ConfSrcDir=TextService\Config
 call :RstDir %ConfDstDir%
-copy %ConfSrcDir%\configs.dat %ConfDstDir%
+copy "%ConfSrcDir%\configs.dat" "%ConfDstDir%"
 
 goto Done
 
 :RstDir
 set Dir=%1
-if exist %Dir% (
-  rd /s /q %Dir%
+if exist "%Dir%" (
+  rd /s /q "%Dir%"
 )
 md %Dir%
 goto :eof
